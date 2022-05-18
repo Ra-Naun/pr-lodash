ff352735e2530fe926ab4852449ab4a9b8e712fb
Ra-Naun <dmitriy.slavskiy@hotger.com>
2022-05-18 18:25:36 +0300

    added array/reverse function

--- src/lib/array/reverse.ts
+++ src/lib/array/reverse.ts
@@ -1,9 +1,12 @@
 export default (array: any[any]): any[any] => {
   const revArr = [];
-  for (let index = array.length - 1; index >= 0; index--) {
+  const { length } = array;
+  for (let index = length - 1; index >= 0; index--) {
     revArr.push(array[index]);
   }
   // OR:
-  // array.forEach(ell => revArr.push(array.pop()));
+  // for (let index = 0; index < length; index++) {
+  //   revArr.push(array.pop());
+  // };
   return revArr;
 }
